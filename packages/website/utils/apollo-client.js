1b45025a47c3505ecb1a43a70cc48e543f02fedd
achraf elghinoussi <afaithraf@gmail.com>
2022-02-01 11:40:26 +0100

    update cache config

--- packages/website/utils/apollo-client.js
+++ packages/website/utils/apollo-client.js
@@ -1,8 +1,23 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        nfts: {
+          keyArgs: false,
+          merge(existing = [], incoming) {
+            return [...existing, ...incoming];
+          },
+        },
+      },
+    },
+  },
+});
+
 const client = new ApolloClient({
   uri: "https://api.thegraph.com/subgraphs/name/achrafismyname/cura-subgraph",
-  cache: new InMemoryCache(),
+  cache: cache,
 });
 
 export default client;
