0bf7b53229b53033c3859e87e391dd3615e9d6e3
achraf elghinoussi <afaithraf@gmail.com>
2022-02-01 09:48:43 +0100

    graphql setup

--- packages/website/pages/_app.tsx
+++ packages/website/pages/_app.tsx
@@ -1,13 +1,18 @@
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
 import { ThemeProvider } from "theme-ui";
 import { theme } from "../theme";
+
+import { ApolloProvider } from "@apollo/client";
+import client from "../utils/apollo-client";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
+      <ApolloProvider client={client}>
+        <Component {...pageProps} />
+      </ApolloProvider>
     </ThemeProvider>
   );
 }
 
-export default MyApp
+export default MyApp;
