f4c688b66f86a62925b0d8616c370c94fa9d660e
AshenI99 <asheniranda@Ashens-MacBook-Air.local>
2022-02-01 22:23:13 +0530

    Added view page

--- packages/website/pages/_app.tsx
+++ packages/website/pages/_app.tsx
@@ -3,15 +3,18 @@ import type { AppProps } from 'next/app'
 import { ThemeProvider } from "theme-ui";
 import { theme } from "../theme";
 import { NearHooksProvider } from '@cura/hooks'
+import { RecoilRoot } from 'recoil'
 
 import '@cura/components/dist/assets/fonts/index.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
-      <NearHooksProvider>
-        <Component {...pageProps} />
-      </NearHooksProvider>
+      <RecoilRoot>
+        <NearHooksProvider>
+          <Component {...pageProps} />
+        </NearHooksProvider>
+      </RecoilRoot>
     </ThemeProvider>
   );
 }
