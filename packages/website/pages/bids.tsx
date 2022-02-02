20283443aadd145c8109c2004fc110c84eddb014
achraf elghinoussi <afaithraf@gmail.com>
2022-02-02 10:36:25 +0100

    bids page

---
+++ packages/website/pages/bids.tsx
@@ -0,0 +1,68 @@
+// @ts-nocheck
+/** @jsxImportSource theme-ui */
+
+import { utils } from "near-api-js";
+import { BiddersBids } from "@cura/components";
+import { useNFTContract, useNearHooksContainer } from "@cura/hooks";
+import { useSetRecoilState } from "recoil";
+import { useQuery, gql } from "@apollo/client";
+
+import Layout from "../containers/Layout";
+import { project } from "../utils/project";
+import { alertMessageState, indexLoaderState } from "../state/recoil";
+
+const CONTRACT_REMOVE_BID_GAS = utils.format.parseNearAmount(`0.00000000020`); // 200 Tgas
+
+const GET_BIDS = gql`
+  query bids($bidder: String) {
+    bids(first: 1, where: { id: $bidder }) {
+      nft
+      bidder
+      amount
+      sell_on_share
+    }
+  }
+`;
+
+const Bids = () => {
+  const { contract } = useNFTContract(project);
+  const { accountId } = useNearHooksContainer();
+  const setIndexLoader = useSetRecoilState(indexLoaderState);
+  const setAlertMessage = useSetRecoilState(alertMessageState);
+
+  const { loading, data, error } = useQuery(GET_BIDS, {
+    variables: {
+      bidder: accountId || "",
+    },
+  });
+
+  setIndexLoader(loading);
+
+  if (error) {
+    setAlertMessage(error.toString());
+    console.error(error);
+  }
+
+  async function removeBid(token_id: string, bidder: string) {
+    setIndexLoader(true);
+    try {
+      await contract.remove_bid(
+        { token_id: token_id, bidder: bidder },
+        CONTRACT_REMOVE_BID_GAS
+      );
+      setIndexLoader(false);
+    } catch (e) {
+      console.error(e);
+      setIndexLoader(false);
+      setAlertMessage(e.toString());
+    }
+  }
+
+  return (
+    <Layout requireAuth={true}>
+      {data && <BiddersBids biddersBids={data?.bids} onRemoveBid={removeBid} />}
+    </Layout>
+  );
+};
+
+export default Bids;
