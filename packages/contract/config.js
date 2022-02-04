f7d19f3387f17971354bfee3265f0b6bb19e842a
achraf elghinoussi <afaithraf@gmail.com>
2022-02-04 10:08:18 +0100

    contract scripts

---
+++ packages/contract/config.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+
+const homedir = require("os").homedir();
+const credentialsPath = require("path").join(homedir, ".near-credentials");
+
+exports.config = {
+  testnet: {
+    networkId: "testnet",
+    nodeUrl: "https://rpc.testnet.near.org",
+    masterAccount: "achtest.testnet",
+    keyPath: credentialsPath + "/testnet/achtest.testnet.json",
+  },
+  mainnet: {
+    networkId: "mainnet",
+    nodeUrl: "https://rpc.mainnet.near.org",
+    masterAccount: "achtest.mainnet",
+    keyPath: credentialsPath + "/mainnet/achtest.mainnet.json",
+  },
+};
+
+const pck = fs.readFileSync("./package.json");
+const pjson = JSON.parse(pck);
+
+// PATCH version is increased by one on each new deploy
+const tmp = pjson.version.split(".");
+const newVersion = `${tmp[0]}.${tmp[1]}.${parseInt(tmp[2]) + 1}`;
+fs.writeFileSync(
+  "./package.json",
+  pck.toString().replace(pjson.version, newVersion)
+);
+
+const version = newVersion.replaceAll(".", "_");
+// A sub account will be created using this name
+exports.CONTRACT_ADDRESS = `cura-${version}`;
+
+// This will be used in init for Contract Metadata
+const metadata = require("./metadata.json");
+exports.METADATA = metadata;
