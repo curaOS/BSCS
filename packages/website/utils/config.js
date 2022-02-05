7453cda24ca4581babb863c98ea63b98a2400d0d
Achraf <afaithraf@gmail.com>
2022-02-05 11:14:01 +0100

    deploy contract with generator

--- packages/website/utils/config.js
+++ packages/website/utils/config.js
@@ -1,3 +1,3 @@
-export const contractAddress = "cura-0_0_1.creatify.testnet";
+export const contractAddress = "cura-0_0_2.creatify.testnet";
 export const graphUri =
   "https://api.thegraph.com/subgraphs/name/curaos/cc-project";
