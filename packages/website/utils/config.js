4be5fea42be0ff96321fb78171c278a23f985c38
AshenI99 <asheniranda@Ashens-MacBook-Air.local>
2022-02-15 17:44:05 +0530

    deploy contract 0.0.11

--- packages/website/utils/config.js
+++ packages/website/utils/config.js
@@ -1,3 +1,3 @@
-export const contractAddress = "cura-0_0_10.creatify.testnet";
+export const contractAddress = "cura-0_0_11.creatify.testnet";
 export const graphUri =
   "https://api.thegraph.com/subgraphs/name/curaos/cc-project";
