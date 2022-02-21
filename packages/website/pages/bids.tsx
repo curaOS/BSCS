ab9dac3f6ca28d0c66ab83ddeb334f6b1ce24a61
Achraf <afaithraf@gmail.com>
2022-02-21 16:01:30 +0100

    fix removeBid

--- packages/website/pages/bids.tsx
+++ packages/website/pages/bids.tsx
@@ -59,7 +59,7 @@ const Bids = () => {
     setIndexLoader(true);
     try {
       await contract.remove_bid(
-        { token_id: token_id, bidder: bidder },
+        { tokenId: token_id, bidder: bidder },
         CONTRACT_REMOVE_BID_GAS
       );
       setIndexLoader(false);
