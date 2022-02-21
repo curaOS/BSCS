afca2e4b7de5299e70e8366d200f109c370c7ef4
Achraf <afaithraf@gmail.com>
2022-02-21 10:03:08 +0100

    add contract package

---
+++ packages/contract/as/cNFT/assembly/models/persistent_tokens.ts
@@ -0,0 +1,292 @@
+import { PersistentSet, PersistentUnorderedMap, u128 } from 'near-sdk-as'
+import { AccountId, TokenId } from '../types'
+import { TokenMetadata } from './persistent_tokens_metadata'
+
+/* @todo metatada is here cause it's returned in enumeration, remove if possible*/
+@nearBindgen
+export class Token {
+
+    /** ID of the token */
+    id: string
+
+    /** ID of the current owner who owns the token */
+    owner_id: string
+
+    /** ID of the creator who created or minted this token */
+    creator_id: string
+
+    /** ID of the previous owner */
+    prev_owner_id: string
+
+    /** Metadata object of the token that describe the token */
+    metadata: TokenMetadata
+}
+
+@nearBindgen
+export class PersistentTokens {
+    private _tmap: PersistentUnorderedMap<TokenId, Token>
+    /** Set is persistent cause it avoids loading the whole Set but just reference */
+    private _amap: PersistentUnorderedMap<AccountId, PersistentSet<TokenId>>
+    private _oset: PersistentSet<AccountId>
+
+    /**
+     * @param prefix A prefix to use for every key of this map
+     */
+    constructor(prefix: string) {
+        this._tmap = new PersistentUnorderedMap<TokenId, Token>(
+            '_tmap' + prefix
+        )
+        this._amap = new PersistentUnorderedMap<
+            AccountId,
+            PersistentSet<TokenId>
+        >('_amap' + prefix)
+
+        this._oset = new PersistentSet<AccountId>('_oset' + prefix)
+    }
+
+
+
+    /**
+     * Get token details of a single token
+     *
+     * **Basic usage example:**
+     *
+     * Assume we need to get the details of the token with token id = `jenny911038`,
+     * ```
+     * const persistent_tokens = new PersistentTokens('pt')
+     * const token = persistent_tokens.get("jenny911038");
+     * ```
+     *
+     * @param token_id ID of token to retrieve from _map
+     * @return Token details object
+     */
+    get(token_id: TokenId): Token {
+        // getSome will throw an error if token_id doesn't exist
+        return this._tmap.getSome(token_id)
+    }
+
+
+
+    /**
+     * Check if the relevant token exists or not
+     *
+     * **Basic usage example:**
+     *
+     * Assume we need to check if the token with token id = `jenny911038` present in the storage,
+     * ```
+     * const persistent_tokens = new PersistentTokens('pt')
+     * const is_token_exists = persistent_tokens.has("jenny911038");
+     * ```
+     *
+     * @param token_id ID of the token to check it's exists or not
+     * @return true if exits, false if not exists
+     */
+    has(token_id: TokenId): bool {
+        return this._tmap.contains(token_id)
+    }
+
+
+
+    /**
+     * Get the IDs of tokens saved in the provided range
+     *
+     * **Basic usage example:**
+     *
+     * Assume we need the IDs of first 5 tokens,
+     * ```
+     * const persistent_tokens = new PersistentTokens('pt')
+     * const token_ids = persistent_tokens.keys(0, 5);
+     * ```
+     *
+     * @param start Start index
+     * @param end End index
+     * @return An array of tokenIds
+     */
+    keys(start: i32, end: i32): TokenId[] {
+        return this._tmap.keys(start, end)
+    }
+
+    /**
+     * TODO not sure _tmap.length can represent up to u128 values
+     */
+
+    /**
+     * Get number of tokens tracked in the contract
+     *
+     * **Basic usage example:**
+     *
+     * Assume contract has 9450 tokens,
+     *
+     * ```
+     * const persistent_tokens = new PersistentTokens('pt')
+     * const tokens_length = persistent_tokens.number_of_tokens();
+     * log(parseInt(tokens_length)) // 9450
+     * ```
+     *
+     * @return Number of tokens present in the contract in `u128` format
+     */
+    get number_of_tokens(): u128 {
+        return u128.from(this._tmap.length)
+    }
+
+
+
+    /**
+     * Get number of tokens saved in the contract for a given account
+     *
+     * **Basic usage example:**
+     *
+     * Assume user with account id = `alice.test.near` has 12 tokens in the contract,
+     *
+     * ```
+     * const persistent_tokens = new PersistentTokens('pt')
+     * const tokens_length_for_user = persistent_tokens.supply_for_owner("alice.test.near");
+     * log(tokens_length_for_user) // "12"
+     * ```
+     *
+     * @param accountId ID of the account to retrieve number of tokens that account owns
+     * @return Number of tokens present for the given account in the contract
+     */
+    supply_for_owner(accountId: AccountId): string {
+        let accountTokenSet = this._amap.get(accountId)
+
+        if (accountTokenSet == null || accountTokenSet.size == 0) {
+            return '0'
+        }
+
+        return accountTokenSet.size.toString()
+    }
+
+
+
+    /**
+     * Get the IDs of tokens saved in the contract for a given account/user
+     *
+     * **Basic usage example:**
+     *
+     * Assume we need the IDs of the tokens that user with account id = `alice.test.near` owns,
+     * ```
+     * const persistent_tokens = new PersistentTokens('pt')
+     * const token_ids_for_user = persistent_tokens.tokens_for_owner("alice.test.near");
+     * ```
+     *
+     * @param accountId ID of the account to retrieve token IDs of the tokens that account/user owns
+     * @return An array of tokenIds that the given account/user owns
+     */
+    tokens_for_owner(accountId: AccountId): TokenId[] {
+        return this._amap.getSome(accountId).values()
+    }
+
+
+
+    /**
+     * Add a new token to the contract
+     *
+     * **Basic usage example:**
+     *
+     * Assume we need add the token `T1` to the contract, and it owns by the user with account id = `alice.test.near`,
+     * ```
+     * const persistent_tokens = new PersistentTokens('pt')
+     * const token = persistent_tokens.add('alice_23456' , T1 , "alice.test.near");
+     * ```
+     *
+     * @param tokenId Id of the new token
+     * @param token Token to be saved to the contract
+     * @param accountId ID of the account that owns the token
+     * @return Saved token
+     */
+    add(tokenId: TokenId, token: Token, accountId: AccountId): Token {
+        this._tmap.set(tokenId, token)
+
+        this._amap.set(
+            accountId,
+            this._addToAccountTokenSet(tokenId, accountId)
+        )
+
+        this._oset.add(accountId)
+
+        return token
+    }
+
+
+
+    /**
+     * Burn a token from the contract
+     *
+     * **Basic usage example:**
+     *
+     * Assume we need burn the token with token id = `alice_23456` from the contract,
+     * ```
+     * const persistent_tokens = new PersistentTokens('pt')
+     * persistent_tokens.burn('alice_23456' , "alice.test.near");
+     * ```
+     *
+     * @param tokenId Id of the token to burn
+     * @param accountId ID of the account that owns the burning token
+     */
+    burn(tokenId: TokenId, accountId: AccountId): void {
+        let token = this._tmap.getSome(tokenId);
+        token.owner_id = '';
+
+        this._tmap.set(tokenId, token);
+
+        this.remove(tokenId, accountId);
+    }
+
+
+
+    /**
+     * Remove a token from a user's token set
+     *
+     * **Basic usage example:**
+     *
+     * Assume we need remove the token with token id = `alice_23456` from the user with account id = `alice.test.near`,
+     * ```
+     * const persistent_tokens = new PersistentTokens('pt')
+     * persistent_tokens.remove('alice_23456' , "alice.test.near");
+     * ```
+     *
+     * @param tokenId Id of the token to remove from
+     * @param accountId ID of the account that owns the removing token
+     */
+    remove(tokenId: TokenId, accountId: AccountId): void {
+
+        this._amap.set(
+            accountId,
+            this._removeFromAccountTokenSet(tokenId, accountId)
+        )
+
+        if (this._amap.getSome(accountId).size == 0) {
+            this._oset.delete(accountId)
+        }
+    }
+
+    private _addToAccountTokenSet(
+        tokenId: TokenId,
+        accountId: AccountId
+    ): PersistentSet<TokenId> {
+        let accountTokenSet = this._amap.get(accountId)
+
+        if (!accountTokenSet) {
+            /** TODO accountId name might be too long, find shorter alternative */
+            accountTokenSet = new PersistentSet<TokenId>('_ats' + accountId)
+        }
+
+        accountTokenSet.add(tokenId)
+
+        return accountTokenSet
+    }
+
+    private _removeFromAccountTokenSet(
+        tokenId: TokenId,
+        accountId: AccountId
+    ): PersistentSet<TokenId> {
+        let accountTokenSet = this._amap.getSome(accountId)
+
+        accountTokenSet.delete(tokenId)
+
+        return accountTokenSet
+    }
+}
+
+export const persistent_tokens = new PersistentTokens('pt')
---
+++ packages/contract/as/cNFT/assembly/models/persistent_tokens_metadata.ts
@@ -0,0 +1,148 @@
+import { PersistentUnorderedMap } from 'near-sdk-as'
+import { TokenId } from '../types'
+
+@nearBindgen
+export class TokenMetadata {
+
+    /** Title of the token */
+    title: string
+
+    /** Token issue or mint date and time in Unix epoch milliseconds */
+    issued_at: string
+
+    /** Number of copies of this set of metadata in existence when token was minted.*/
+    copies: u8
+
+    /** URL to associated media for the token. Preferably to decentralized, content-addressed storage */
+    media: string
+
+    /** Anything extra the token wants to store on-chain. Can be stringified JSON. */
+    extra: string
+
+    /** Token description */
+    description: string
+
+    /** Base64-encoded sha256 hash of content referenced by the `media` field. Required if `media` is included. */
+    media_hash: string
+
+    /** Token expiring date and time in Unix epoch milliseconds */
+    expires_at: string
+
+    /** Token starts to being valid date and time in Unix epoch milliseconds */
+    starts_at: string
+
+    /** Token last updated date and time in Unix epoch milliseconds */
+    updated_at: string
+
+    /** A link to a valid JSON file containing various keys offering supplementary details on the token */
+    reference: string
+
+    /** Base64-encoded sha256 hash of the JSON file contained in the reference field */
+    reference_hash: string
+
+    /** Base64-encoded sha256 hash of content referenced by the `media` field if the media has an animation. */
+    media_animation: string
+}
+
+
+
+/**
+ * @hidden
+ */
+export function defaultTokenMetadata(): TokenMetadata {
+    return {
+        title: '',
+        issued_at: '',
+        copies: 1,
+        media: '',
+        extra: '',
+        description: '',
+        media_hash: '',
+        expires_at: '',
+        starts_at: '',
+        updated_at: '',
+        reference: '',
+        reference_hash: '',
+        media_animation: ''
+    }
+}
+
+@nearBindgen
+export class PeristentTokenMetadata {
+    /** @todo explain this structure, same in PersistentTokens */
+    private _tmmap: PersistentUnorderedMap<TokenId, TokenMetadata>
+
+    /**
+     * @param prefix A prefix to use for every key of this map
+     */
+    constructor(prefix: string) {
+        this._tmmap = new PersistentUnorderedMap<TokenId, TokenMetadata>(
+            '_tmmap' + prefix
+        )
+    }
+
+
+    /**
+     * Get token metadata for a given token ID
+     *
+     * **Basic usage example:**
+     *
+     * Assume we need to get the metadata of the token with token id = `jenny911038`,
+     * ```
+     * const persistent_tokens_metadata = new PeristentTokenMetadata('ptm')
+     * const metadata = persistent_tokens_metadata.get("jenny911038");
+     * ```
+     *
+     * @param tokenId ID of token to retrieve metadata
+     * @return Token Metadata object
+     */
+    get(tokenId: TokenId): TokenMetadata {
+        return this._tmmap.getSome(tokenId)
+    }
+
+
+
+    /**
+     * Get the IDs of tokens saved in the provided range
+     *
+     * **Basic usage example:**
+     *
+     * Assume we need the IDs of first 5 tokens,
+     * ```
+     * const persistent_tokens_metadata = new PeristentTokenMetadata('ptm')
+     * const token_ids = persistent_tokens_metadata.keys(0, 5);
+     * ```
+     *
+     * @param start Start index
+     * @param end End index
+     * @return An array of tokenIds
+     */
+    keys(start: i32, end: i32): TokenId[] {
+        return this._tmmap.keys(start, end)
+    }
+
+
+
+    /**
+     * Add metadata of a token
+     *
+     * **Basic usage example:**
+     *
+     * Assume we need add the metadata object `TM1` to the token with token id = `jenny911038`,
+     * ```
+     * const persistent_tokens_metadata = new PeristentTokenMetadata('ptm')
+     * const metadata = persistent_tokens_metadata.add('jenny911038' , TM1 );
+     * ```
+     *
+     * @param tokenId Id of the token that need to save metadata
+     * @param tokenMetadata Metadata object to be saved
+     * @return Saved token metadata object
+     */
+    add(tokenId: TokenId, tokenMetadata: TokenMetadata): TokenMetadata {
+        this._tmmap.set(tokenId, tokenMetadata)
+
+        return tokenMetadata
+    }
+}
+
+export const persistent_tokens_metadata = new PeristentTokenMetadata('ptm')
