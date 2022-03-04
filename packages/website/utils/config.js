e1c748f2907a33b340ed2550561e36075ed59e49
AshenI99 <asheniranda@Ashens-MacBook-Air.local>
2022-03-04 22:35:05 +0530

    Test 1.5.3

--- packages/website/utils/config.js
+++ packages/website/utils/config.js
@@ -1,3 +1,3 @@
-export const contractAddress = "test4.ashen99.testnet";
+export const contractAddress = "test5.ashen99.testnet";
 export const graphUri =
   "https://api.thegraph.com/subgraphs/name/asheni99/graph_test";
