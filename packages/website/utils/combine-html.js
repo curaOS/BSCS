c65f4da274748a212cdec8b9ba70be20de464d23
Achraf <afaithraf@gmail.com>
2022-03-04 11:02:46 +0100

    update code scripts

--- packages/website/utils/combine-html.js
+++ packages/website/utils/combine-html.js
@@ -1,19 +1,31 @@
 export function combineHTML(
-    paramsScript,
-    packagesScript,
-    renderScript,
-    styleCSS
+  paramsScript,
+  packagesScript,
+  renderScript,
+  styleCSS
 ) {
-    return `
+  return `
 		<html>
   			<head>
     				<meta charset="utf-8" />
 				${paramsScript}
-				${atob(packagesScript)}
+				${handlePackages(packagesScript)}
 				${atob(renderScript)}
 				${atob(styleCSS)}
   			</head>
 		</html>
 	
-	`
+	`;
+}
+
+export function handlePackages(packagesScript) {
+  let html = "";
+  for (const key in packagesScript) {
+    const scriptUrl =
+      packagesScript[key].decentralized_storage == ""
+        ? packagesScript[key].centralized_storage
+        : packagesScript[key].decentralized_storage;
+    html += `<script src="${scriptUrl}"></script>`;
+  }
+  return html;
 }
