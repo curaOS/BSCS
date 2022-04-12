afa5ff9b90686e98993487ce0c233f9bab3f5ff4
Yassine <yassinesania@gmail.com>
2022-04-12 13:08:55 +0200

    Support deployment to same version
    + add to workspace scripts

--- packages/scripts/deploy.js
+++ packages/scripts/deploy.js
@@ -1,7 +1,11 @@
+const { program } = require("commander");
 const nearAPI = require("near-api-js");
-const BN = require("bn.js");
 const fs = require("fs").promises;
 const configFile = require("./config.js");
+const { homedir } = require('os');
+const {KeyPair} = require("near-api-js");
+
+
 
 let config;
 let masterAccount;
@@ -11,6 +15,56 @@ let keyStore;
 let near;
 let contractAddress;
 
+program
+  .option("-p, --new", "Deploy to same minor version contract")
+  .option("-m, --same", "Deploy to new minor version contract");
+
+program.parse(process.argv);
+
+const options = program.opts();
+
+if (options.new) {
+  console.log("Deploying to new minor version...");
+  deployNewContract();
+}
+
+if (options.same)   {
+    console.log("Deploying to same minor version...");
+    deploySameContract();
+  }
+
+async function deployNewContract() {
+  await initNear();
+  const contract = await fs.readFile(configFile.CONTRACT);
+
+  const version = await configFile.newVersion();
+  contractAddress = version + "." + config.masterAccount;
+
+  const newKeyPair = KeyPair.fromRandom('ed25519');
+  const newPublicKey = newKeyPair.publicKey.toString();
+  await keyStore.setKey(config.networkId, contractAddress, newKeyPair);
+
+  await masterAccount.createAndDeployContract(contractAddress, newPublicKey, contract, nearAPI.utils.format.parseNearAmount("1"));
+
+  console.log("Contract deployed to: " + contractAddress);
+
+  init();
+}
+
+async function deploySameContract() {
+  await initNear();
+  const contract = await fs.readFile(configFile.CONTRACT);
+
+  const version = await configFile.currentVersion();
+  contractAddress = version + "." + config.masterAccount;
+
+  const sameAccount = await near.account(contractAddress);
+
+  sameAccount.deployContract(contract);
+
+  console.log("Contract deployed to: " + contractAddress);
+}
+
 async function initNear() {
   config = await configFile.networkConfig();
 
@@ -19,38 +73,16 @@ async function initNear() {
     keyFile.secret_key || keyFile.private_key
   );
   pubKey = masterKey.getPublicKey();
-  keyStore = new nearAPI.keyStores.InMemoryKeyStore();
-  keyStore.setKey(config.networkId, config.masterAccount, masterKey);
+  keyStore = new nearAPI.keyStores.UnencryptedFileSystemKeyStore(`${homedir()}/.near-credentials`);
+  await keyStore.setKey(config.networkId, config.masterAccount, masterKey);
   near = await nearAPI.connect({
-    deps: {
-      keyStore,
-    },
+    keyStore,
     networkId: config.networkId,
     nodeUrl: config.nodeUrl,
   });
   masterAccount = new nearAPI.Account(near.connection, config.masterAccount);
+
   console.log("Finish init NEAR");
-}
-
-async function deploy() {
-  await initNear();
-
-  const contract = await fs.readFile(configFile.CONTRACT);
-
-  const version = await configFile.newVersion();
-
-  contractAddress = version + "." + config.masterAccount;
-
-  const response = await masterAccount.createAndDeployContract(
-    contractAddress,
-    pubKey,
-    contract,
-    new BN(9).pow(new BN(25))
-  );
-
-  console.log("Contract deployed to: " + contractAddress);
-
-  init();
 }
 
 async function init() {
@@ -74,5 +106,3 @@ async function init() {
 
   configFile.updateContractAddress(contractAddress);
 }
-
-deploy();
